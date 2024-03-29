871bd6f3030397de1d1faf359ce8a1d95e8fa42d
Oliver Sharif <oliver@sharif-m.de>
2024-03-29 22:19:09 +0100

    changed to flags

--- ArgsParser/argsparser.go
+++ ArgsParser/argsparser.go
@@ -1,44 +1,26 @@
 package ArgsParser
 
-import (
-	"os"
-	"strings"
-)
+import "flag"
 
 // ArgsParser struct
 type ArgsParser struct {
-	Options map[string]string
+	Ip       *string
+	Port     *int
+	Secure   *bool
+	CertFile *string
+	KeyFile  *string
 }
 
 // NewArgsParser returns a new ArgsParser // TODO: SUBJECT TO HEAVY CHANGE -- use flags instead
 func NewArgsParser() *ArgsParser {
-	m := make(map[string]string)
+	// Create an new ArgsParser
+	ap := &ArgsParser{}
 
-	// Add the options
-	m["IP"] = "0.0.0.0"
-	m["PORT"] = "8080"
-	m["SECURE"] = ""
-	m["CERTFILE"] = ""
-	m["KEYFILE"] = ""
-	return &ArgsParser{Options: m}
-}
-
-// ParseArgs parses the arguments, options must start with -- and have a value delimited with =
-func (a *ArgsParser) ParseArgs() bool {
-	args := os.Args
-	for i := 1; i < len(args); i++ {
-		if len(args[i]) > 2 && args[i][0:2] == "--" {
-			split := strings.Split(args[i][2:], "=")
-			if len(split) == 2 {
-				// Check if the option is valid
-				if _, ok := a.Options[split[0]]; !ok {
-					return false
-				}
-				a.Options[split[0]] = split[1]
-			}
-		} else {
-			return false
-		}
-	}
-	return true
+	// Get the flags
+	ap.Ip = flag.String("ip", "0.0.0.0", "The IP to bind the server to")
+	ap.Port = flag.Int("port", 8080, "The port to bind the server to")
+	ap.Secure = flag.Bool("secure", false, "Use HTTPS")
+	ap.CertFile = flag.String("certfile", "", "The path to the certificate file")
+	ap.KeyFile = flag.String("keyfile", "", "The path to the key file")
+	return ap
 }
--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"VectoriaDB/ArgsParser"
 	"VectoriaDB/Server"
-	"log"
-	"strconv"
 )
 
 func main() {
@@ -13,25 +11,9 @@ func main() {
 
 	// Create the ArgsParser
 	argsParser := ArgsParser.NewArgsParser()
-	if argsParser.ParseArgs() == false {
-		log.Fatal("Arguments are in the wrong format")
-	}
-
-	// Check if SECURE is set
-	if argsParser.Options["SECURE"] == "" {
-		b = false
-	} else {
-		b = true
-	}
-
-	// Port must be a number so we convert it
-	port, err := strconv.Atoi(argsParser.Options["PORT"])
-	if err != nil {
-		log.Fatal("Cannot convert Port - please check the format of the port")
-	}
 
 	// Start the Server
-	server := Server.NewServer(argsParser.Options["IP"], port, argsParser.Options["CERTFILE"],
-		argsParser.Options["KEYFILE"], b, argsParser)
+	server := Server.NewServer(*argsParser.Ip, *argsParser.Port, *argsParser.CertFile,
+		*argsParser.KeyFile, b, argsParser)
 	server.Start()
 }
