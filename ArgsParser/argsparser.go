4c298434f011c09f308c3cde55d7700d77fa7c12
Oliver Sharif <oliver@sharif-m.de>
2024-04-04 17:49:16 +0200

    added ApiKey generation startup

--- ArgsParser/argsparser.go
+++ ArgsParser/argsparser.go
@@ -1,14 +1,18 @@
 package ArgsParser
 
-import "flag"
+import (
+	"VreeDB/ApiKeyHandler"
+	"flag"
+)
 
 // ArgsParser struct
 type ArgsParser struct {
-	Ip       *string
-	Port     *int
-	Secure   *bool
-	CertFile *string
-	KeyFile  *string
+	Ip           *string
+	Port         *int
+	Secure       *bool
+	CertFile     *string
+	KeyFile      *string
+	CreateApiKey *bool
 }
 
 // Ap is a global ArgsParser
@@ -24,4 +28,20 @@ func init() {
 	Ap.Secure = flag.Bool("secure", false, "Use HTTPS")
 	Ap.CertFile = flag.String("certfile", "", "The path to the certificate file")
 	Ap.KeyFile = flag.String("keyfile", "", "The path to the key file")
+	Ap.CreateApiKey = flag.Bool("createapikey", false, "Create a new API key")
+
+	// Parse
+	flag.Parse()
+
+	// if CreateApiKey is true, then the program will create a new API and show it in the console
+	if *Ap.CreateApiKey {
+		// Create a new API key
+		apiKey, err := ApiKeyHandler.ApiHandler.CreateApiKey()
+		if err != nil {
+			panic(err)
+		} else {
+			// Show the API key
+			println("API Key: " + apiKey)
+		}
+	}
 }
