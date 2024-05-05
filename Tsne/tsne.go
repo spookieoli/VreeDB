2fc93ca6428d5ef95a7198233443526f79bb1262
Oliver Sharif <spockmob@gmail.com>
2024-05-05 20:01:31 +0200

    added main methods

--- Tsne/tsne.go
+++ Tsne/tsne.go
@@ -1,14 +1,69 @@
 package Tsne
 
-import (
-	"VreeDB/Vector"
-	"sync"
-)
+// TSNE is a struct that represents the t-Distributed Stochastic Neighbor Embedding algorithm.
+// It is used for dimensionality reduction and visualization of high-dimensional data.
+//
+// The TSNE struct has the following fields:
+// - perplexity: The perplexity hyperparameter for the TSNE algorithm.
+// - learningRate: The learning rate or step size for the TSNE algorithm.
+// - maxIterations: The maximum number of iterations for the TSNE algorithm.
+type TSNE struct {
+	perplexity    float64
+	learningRate  float64
+	dimensions    int // represent the number of dimensions in the output space
+	maxIterations int
+}
 
-type Tsne struct {
-	Vectors         *Vector.Vector
-	Mutex           *sync.RWMutex
-	Projection      *Vector.Vector
-	Iterations      int
-	TargetDimension int
+// NewTSNE is a function that creates a new instance of the TSNE struct with the provided parameters.
+// It takes the perplexity, learning rate, maximum iterations, and dimensions as inputs.
+// It returns a pointer to the newly created TSNE struct.
+//
+// Example usage:
+// tsne := NewTSNE(30.0, 0.1, 1000, 2)
+// tsne.PerformTSNE()
+//
+// Parameters:
+// - perplexity: The perplexity hyperparameter for the TSNE algorithm.
+// - learningRate: The learning rate or step size for the TSNE algorithm.
+// - maxIterations: The maximum number of iterations for the TSNE algorithm.
+// - dimensions: The number of dimensions in the output space.
+//
+// Returns:
+// A pointer to a new TSNE instance.
+func NewTSNE(perplexity, learninrate float64, maxiterations, dimensions int) *TSNE {
+	return &TSNE{perplexity: perplexity, learningRate: learninrate, maxIterations: maxiterations, dimensions: dimensions}
+}
+
+// PerformTSNE performs the t-Distributed Stochastic Neighbor Embedding algorithm
+// on the given data using the current configuration of the TSNE instance.
+//
+// This method reduces the dimensionality of high-dimensional data and produces
+// a lower-dimensional representation suitable for visualization.
+//
+// The method does not return any value and modifies the TSNE instance's internal state.
+// To access the result of the embedding, use the GetEmbedding method.
+//
+// Note that before calling PerformTSNE, you should set the desired configuration
+// parameters of the TSNE instance, such as perplexity, learning rate, and maximum iterations.
+// If these parameters are not set, default values will be used.
+//
+// Example usage:
+//    tsne := NewTSNE()
+//    tsne.PerformTSNE()
+//
+// After calling PerformTSNE, you can obtain the reduced-dimensional embedding
+// using the GetEmbedding method:
+//    embedding := tsne.GetEmbedding()
+//
+// The PerformTSNE method utilizes the current configuration parameters to run the algorithm,
+// and the resulting embedding is stored in the TSNE instance's internal state.
+//
+// Note that calling PerformTSNE will overwrite any previous embedding stored in the TSNE instance.
+//
+// For more information on the t-SNE algorithm, refer to the original paper by L.J.P. van der Maaten and G.E. Hinton:
+// "Visualizing Data Using t-SNE" (2008).
+// DOI: 10.1109/TVCG.2008.167
+
+func (t *TSNE) PerformTSNE() {
+
 }
