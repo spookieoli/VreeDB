2f29862603a52b3ffc0df55a731afcd1a7ca2bb5
Oliver Sharif <spockmob@gmail.com>
2024-06-22 20:04:01 +0200

    Refactor prediction correctness check in NeuralNet
    Removed unnecessary labeling and improved the check of the correctness in predictions. Implemented a boolean flag to track whether a prediction is correct or not, which simplifies code flow by avoiding the 'continue OUTER' statement used previously. This makes the code more readable and maintainable.

--- NN/NeuralNet.go
+++ NN/NeuralNet.go
@@ -254,7 +254,6 @@ func (n *Network) Accuracy(testData [][]float64, testLabels [][]float64) float64
 	var correctCount float64
 
 	for i, data := range testData {
-	OUTER:
 		prediction := n.Predict(data)
 		// all values under 0.5 must be set to 0 and all values above 0.5 must be set to 1
 		for i, v := range prediction.([]float64) {
@@ -265,13 +264,17 @@ func (n *Network) Accuracy(testData [][]float64, testLabels [][]float64) float64
 			}
 		}
 
+		check := true
 		// Check if the prediction is correct
 		for _, pred := range prediction.([]float64) {
 			if pred != testLabels[i][0] {
-				continue OUTER
+				check = false
 			}
 		}
-		correctCount++
+		// If the prediction is correct, increment the correctCount
+		if check {
+			correctCount++
+		}
 	}
 
 	// The accuracy is the number of correct predictions divided by the total number of predictions.
