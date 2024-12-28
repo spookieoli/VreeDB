59092489a4ae61b6e55f7b880c71d707e505dc19
Oliver Sharif <spockmob@gmail.com>
2024-12-28 19:44:06 +0100

    Add distance function and validation to Ac structure
    Integrated a distance calculation function into the `Ac` structure, with support for AVX, NEON, and default methods depending on system configuration. Added logic to validate node insertion based on minimum distance thresholds, enhancing spatial distribution within the collection.

--- Collection/aces.go
+++ Collection/aces.go
@@ -3,6 +3,7 @@ package Collection
 import (
 	"VreeDB/ArgsParser"
 	"VreeDB/Node"
+	"VreeDB/Utils"
 	"VreeDB/Vector"
 	"sync"
 )
@@ -10,33 +11,68 @@ import (
 // Ac is an advanced cluster - this cluster is build from a small subset of collection Nodes
 type Ac struct {
 	Nodes        *Node.Node
+	Space        []*Node.Node
 	Mut          *sync.RWMutex
 	Collection   *Collection
 	Count        int64
 	Distribution float64
+	DistanceFunc func(*Vector.Vector, *Vector.Vector) (float64, error)
 }
 
 // NewAc returns a new Ac
 func NewAc(collection *Collection) *Ac {
 	// Create a new Ac
-	return &Ac{Nodes: &Node.Node{Depth: 0}, Mut: &sync.RWMutex{}, Collection: collection, Count: 0, Distribution: *ArgsParser.Ap.ACDistribution}
+	ac := &Ac{Nodes: &Node.Node{Depth: 0}, Mut: &sync.RWMutex{}, Collection: collection, Count: 0, Distribution: *ArgsParser.Ap.ACDistribution}
+
+	// Get actual Distance func, depends on AVX enabled or not
+	if *ArgsParser.Ap.AVX256 {
+		ac.DistanceFunc = Utils.Utils.EuclideanDistanceAVX256
+	} else {
+		ac.DistanceFunc = Utils.Utils.EuclideanDistance
+	}
+
+	// If we are on ARM, use Neon
+	if *ArgsParser.Ap.Neon {
+		ac.DistanceFunc = Utils.Utils.EuclideanDistanceNEON
+	}
+
+	return ac
 }
 
 // Insert inserts a Node into the Ac
 func (a *Ac) Insert(node *Node.Node) {
-	a.Mut.Lock()
-	defer a.Mut.Unlock()
 	// check if we have already enough ACES points
 	if a.Count >= int64(float64(a.Collection.GetNodeCount())**ArgsParser.Ap.ACDistribution) {
 		return
 	}
-	a.Nodes.ACESInsert(node)
-	a.Count++
+
+	// Check if the distance is greater to all added nodes
+	if a.chkDistances(node.Vector) {
+		a.Nodes.ACESInsert(node)
+		a.Space = append(a.Space, node)
+		a.Count++
+	}
 }
 
 // chkDistances checks the distances of the nodes in the Ac
 func (a *Ac) chkDistances(v *Vector.Vector) bool {
 	a.Mut.RLock()
 	defer a.Mut.RUnlock()
-	return false
+	// Get the Diagonal Length of the Collection
+	dl := a.Collection.DiagonalLength * *ArgsParser.Ap.ACESMindDist
+
+	// Check every node in the Ac - if the distance to one of the Vectors is smaller than ACESMindDist, break
+	// For now, we will only use euclidean distance
+	for _, node := range a.Space {
+		dist, err := a.DistanceFunc(node.Vector, v)
+		if err != nil {
+			return false
+		}
+
+		// If the distance is greater or equal than the minimum distance, return continue
+		if dist >= dl {
+			continue
+		}
+	}
+	return true
 }
